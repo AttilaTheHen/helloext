124f50702fc63f06f4490d972fda0423044e2087
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-07-31 13:30:04 -0700

    feat: Add backend functionality to app

---
+++ helloext-server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const http = require('http');
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res) => {
+    res.sendFile('index.html', { root: '../helloext-app/' });
+});
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    // eslint-disable-next-line
+    console.log('server running on', server.address().port);
+});
