feab206af91a9855fb397772a894ef73e45d844c
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-08-02 12:22:38 -0700

    feat: Add db-client file to connect to Postgres database and tweak server.js to use db-client file

--- helloext-server/server.js
+++ helloext-server/server.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const morgan = require('morgan');
-
-const http = require('http');
 
 app.use(morgan('dev'));
 app.use(express.static('../helloext-app'));
+app.use(cors());
 app.use(express.json());
+
+const client = require('./db-client');
 
 app.use((req, res) => {
     res.sendFile('index.html', { root: '../helloext-app/' });
 });
 
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-
-server.listen(port, () => {
-    // eslint-disable-next-line
-    console.log('server running on', server.address().port);
-});
+// eslint-disable-next-line
+app.listen(PORT, () => console.log('server running...'));
